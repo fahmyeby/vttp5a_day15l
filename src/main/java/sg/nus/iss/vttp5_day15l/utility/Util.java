8f73063e69da541f01367da00174aa10cc00d9eb
fahmyeby <fahmyebyyussoff@yahoo.com.sg>
2024-11-22 11:26:56 +0800

    added redis configs

---
+++ src/main/java/sg/nus/iss/vttp5_day15l/utility/Util.java
@@ -0,0 +1,9 @@
+package sg.nus.iss.vttp5_day15l.utility;
+
+public class Util {
+
+    public static final String template01 = "template01";
+
+    public static final String template02 = "template02";
+    
+}
