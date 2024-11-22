d010f9a48b1e810b6679136c563c3bcc130f9062
fahmyeby <fahmyebyyussoff@yahoo.com.sg>
2024-11-22 11:00:51 +0800

    first commit

---
+++ src/main/java/sg/nus/iss/vttp5_day15l/Vttp5Day15lApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.vttp5_day15l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5Day15lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5Day15lApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/iss/vttp5_day15l/Vttp5Day15lApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.vttp5_day15l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5Day15lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
